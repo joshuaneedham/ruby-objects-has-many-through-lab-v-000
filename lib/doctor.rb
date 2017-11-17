9e6baf19e000af9e286c0fc3c3974f956c77d986
Joshua Needham <joshuabneedham@gmail.com>
2017-11-17 22:45:57 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,17 +1,12 @@
 class Doctor
   attr_accessor :name, :patients
 
-  @@appointments = []
-
   def initialize(name)
     @name = name
     @patients = patients
-    @@appointments
   end
 
   def add_appointment
-    @@appointments << appointment
-    appointment.doctor = self
   end
 
   def appointments
