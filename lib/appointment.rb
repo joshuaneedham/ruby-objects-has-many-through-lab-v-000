6a4138dbf073c534082406db53922bc0bd3e88f4
Joshua Needham <joshuabneedham@gmail.com>
2017-11-17 23:57:46 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,14 +4,7 @@ class Appointment
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    @patient = patient
+    doctor.add_appointment(self)
   end
 
-  def patient
-    @patient
-  end
-
-  def doctor
-    @doctor
-  end
 end
