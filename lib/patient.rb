2c95f2c5c7c881032b7545deea58ab89427b2148
Joshua Needham <joshuabneedham@gmail.com>
2017-11-17 22:42:32 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,15 +3,15 @@ class Patient
 
   def initialize(name)
     @name = name
+    @appointments = appointments
   end
 
   def appointments
-    @appointments
   end
 
   def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+    @appointments << appointments
+    appointments.patient = self
   end
 
   def doctors
