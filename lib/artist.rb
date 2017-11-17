60d7224edd59d96c4386bf2afce8cd2e63d7f311
Joshua Needham <joshuabneedham@gmail.com>
2017-11-17 21:59:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,11 @@
 class Artist
   attr_accessor :name, :songs, :genre
 
-  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
     @genre = genre
-  end
-
-  def self.all
-    @@all
   end
 
   def add_song(song)
